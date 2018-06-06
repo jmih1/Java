39dcd27d8aea6a5f7a4d4ac42d41b52b9b5d9231
jmih1 <brownbennet@gmail.com>
2018-06-06 15:59:03 +0000

    test file

---
+++ Example1.java
@@ -0,0 +1,23 @@
+// ******************************************
+// Example1
+// written by Cannon
+// 
+// *******************************************
+
+
+public class Example1
+{
+    public static void main(String[] args)
+    {
+        int a;
+        a=4; // declare a as an int and intitialize it 
+        int b=2; // declare b as an int and intitialize it
+        String name = "Joe Lion"; // declare and initialize name 
+        double c = 7/b; // do integer division
+	
+        // print the results
+        System.out.println("Hey " + name + ", c is " + c + "!");
+
+    } // end of the main method
+
+} // end of the class
